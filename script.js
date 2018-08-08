8d26421d0f21c0eb285e01182fb874e2958adcc7
ginjake <hsginjake123@gmail.com>
2018-08-08 23:26:56 +0900

    画像のエクスポートとインポート

--- script.js
+++ script.js
@@ -116,6 +116,60 @@ function dumpCanvas () {
     document.getElementById("output").value = prefix + chars.join(",\n") + suffix;
 }
 
+var file = document.getElementById('file');
+var canvas = document.getElementById('canvas');
+
+var uploadImgSrc;
+
+// Canvasの準備
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+var ctx = canvas.getContext('2d');
+
+function loadLocalImage(e) {
+    // ファイル情報を取得
+    var fileData = e.target.files[0];
+
+    // 画像ファイル以外は処理を止める
+    if(!fileData.type.match('image.*')) {
+        alert('画像を選択してください');
+        return;
+    }
+
+    // FileReaderオブジェクトを使ってファイル読み込み
+    var reader = new FileReader();
+    // ファイル読み込みに成功したときの処理
+    reader.onload = function() {
+        // Canvas上に表示する
+        uploadImgSrc = reader.result;
+        canvasDraw();
+    }
+    // ファイル読み込みを実行
+    reader.readAsDataURL(fileData);
+}
+
+// ファイルが指定された時にloadLocalImage()を実行
+file.addEventListener('change', loadLocalImage, false);
+
+
+function chgImg()
+{
+  var png = canvas.toDataURL();
+  document.getElementById("newImg").src = png;
+}
+
+// Canvas上に画像を表示する
+function canvasDraw() {
+    // canvas内の要素をクリアする
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Canvas上に画像を表示
+    var img = new Image();
+    img.src = uploadImgSrc;
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, CANVAS_WIDTH, this.height * (CANVAS_WIDTH / this.width));
+    }
+}
+
 document.getElementById("in").onclick = readScript;
 document.getElementById("out").onclick = dumpCanvas;
 document.getElementById("pen").onclick = pen;
